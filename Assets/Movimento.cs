a531812572d7df6a21d7799a88b59d3f4978343a
Leon S Kennedy <samuelkhouri3@gmail.com>
2026-03-02 11:15:23 -0300

    fisica do joguin

---
+++ Assets/Movimento.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+
+public class Movimento : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(0 , 0, 0.05f);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, 0.05f, 0);
+        }
+        if (Input.GetKey("x"))
+        {
+            transform.Translate(0, -0.05f, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(0, 0, -0.05f);
+        }
+
+       }
+}
